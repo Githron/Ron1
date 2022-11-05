8392d1b87af7c0735745602c29ae1a32dc79f35b
Githron <103646138+Githron@users.noreply.github.com>
2022-11-05 06:37:51 +0000

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Dec 29, 2022',
+  birthdate: 'Dec 29, 2021',
   name: 'Wella'
 };
  
