4d31d25a299515b04451c05f9fe16b602a234b56
Githron <103646138+Githron@users.noreply.github.com>
2022-11-05 06:33:56 +0000

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,73 @@
+// console.log("I Love You")
+
+/*
+const prompt = require('prompt-sync')();
+
+var fname = prompt("enter your first name : ");
+var lname = prompt("enter your last name : ");
+
+console.log("your full name is",fname,lname);
+*/
+
+// console.error("hello you");
+
+/* 
+let todo1 = 99;
+console.log(typeof todo1);   // Result is: "number"
+*/
+
+/*
+let month = 'November';
+let dayOfMonth = '9';
+let year = '2021';
+console.log(month + ' ' + dayOfMonth + ', ' + year);
+*/
+
+/*
+let result1 = 4 + 5 * 3;
+console.log(result1);   // Result is: 19
+let result2 = (4 + 5) * 3;
+console.log(result2);   // Result is: 27
+*/
+
+/*
+let age = 25;
+let message = 'I am ' + age + ' years old';
+console.log(typeof message);    // Result is: "string".
+                                // Combining a string and a number results in a string.
+*/
+
+
+
+/*
+// use only in browser
+let button = document.createElement('button');
+button.innerText = 'Click me';
+document.body.appendChild(button);
+*/
+
+
+
+/*
+// Create a function named greeting that takes 1 parameter firstName and uses it to console.log a message saying "hello".
+function greeting(firstName) {
+    console.log('hello ' + firstName);
+  }
+  greeting('Simon');
+  */
+
+
+
+
+// Write a function named toUpper that converts 1 string parameter str to uppercase and console.log the result. Use Google to find the code for converting a string to uppercase in Javascript.
+/*
+function toUpper(str) {
+    console.log(str.toUpperCase());
+  }
+  toUpper('Simon');
+  */
+
+
+
+
+  
